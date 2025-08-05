0dcffd493cd61f6f27f1435d598a1d00feea1811
WaseekZee <waseek443@gmail.com>
2025-08-05 23:22:34 +0530

    All done

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -44,8 +44,7 @@ def login():
 
 
 @auth_bp.route('/logout')
-@login_required
 def logout():
-    logout_user()
-    flash("Logged out successfully", "success")
+    session.clear()
+    flash('Logged out successfully.', 'info')
     return redirect(url_for('auth_bp.login'))
